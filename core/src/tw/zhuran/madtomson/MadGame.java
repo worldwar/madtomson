99a6221f12c49a0c9d3eff9c5d1e16397f238fe8
Zhu Ran <zhuran94@163.com>
2018-03-27 16:17:01 +0800

    draw a piece on board

--- core/src/tw/zhuran/madtomson/MadGame.java
+++ core/src/tw/zhuran/madtomson/MadGame.java
@@ -3,17 +3,18 @@ package tw.zhuran.madtomson;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import tw.zhuran.madtom.domain.Piece;
+import tw.zhuran.madtom.domain.Pieces;
+import tw.zhuran.madtomson.core.Client;
 
 public class MadGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Client client;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,13 +22,19 @@ public class MadGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.disableBlending();
+		draw(batch, client);
 		batch.end();
 	}
-	
+
+	private void draw(SpriteBatch batch, Client client) {
+		Piece piece = Pieces.ERTONG;
+		batch.setColor(1, 1, 1, 1);
+		P.sprite(piece).draw(batch);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/tw/zhuran/madtomson/P.java
@@ -0,0 +1,48 @@
+package tw.zhuran.madtomson;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.github.underscore.$;
+import com.github.underscore.Block;
+import tw.zhuran.madtom.domain.Piece;
+import tw.zhuran.madtom.domain.Pieces;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P {
+    public static Texture PIECES_TEXTURE = new Texture("mahjong.png");
+    public static Map<Piece, Sprite> PIECES = new HashMap<Piece, Sprite>();
+
+    public static int TOP = 0;
+    public static int LEFT = 0;
+    public static int ROW_MARGIN = 0;
+    public static int COL_MARGIN = 0;
+    public static int PIECE_WIDTH = 100;
+    public static int PIECE_HEIGHT = 125;
+
+    static {
+        $.each(Pieces.ALL, new Block<Piece>() {
+            @Override
+            public void apply(Piece x) {
+                PIECES.put(x, makePiece(x));
+            }
+        });
+    }
+
+    public static Sprite makePiece(Piece piece) {
+        int row = piece.getKind().ordinal();
+        int col  = piece.getIndex() - 1;
+        int x = LEFT + col * (PIECE_WIDTH + COL_MARGIN);
+        int y = TOP + row * (PIECE_HEIGHT+ ROW_MARGIN);
+        Sprite sprite = new Sprite(PIECES_TEXTURE, x, y, PIECE_WIDTH, PIECE_HEIGHT);
+        sprite.setColor(1, 1, 1, 1);
+        return sprite;
+    }
+
+    public static Sprite sprite(Piece piece) {
+        return PIECES.get(piece);
+    }
+}
